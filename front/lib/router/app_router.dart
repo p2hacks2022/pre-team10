ee2c278d253bf432b790641d71c2de72954d1080
yama-yeah <b1021091@fun.ac.jp>
2022-12-12 22:50:26 +0900

    add providers for app router

--- front/lib/router/app_router.dart
+++ front/lib/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:front/ui/main/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../ui/login/login_screen.dart';
 import 'auth_guard.dart';
@@ -17,3 +18,7 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   AppRouter({required super.authGuard});
 }
+
+final appRouterProvider = Provider<AppRouter>((ref) {
+  return AppRouter(authGuard: ref.watch(authGuardProvider));
+});
