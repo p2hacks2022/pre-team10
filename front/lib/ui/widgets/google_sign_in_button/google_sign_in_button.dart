48eb2b70bbd677aeb75c75a348508d0079a5a649
yama-yeah <b1021091@fun.ac.jp>
2022-12-12 20:19:57 +0900

    add google sign in button

--- front/lib/ui/widgets/google_sign_in_button/google_sign_in_button.dart
+++ front/lib/ui/widgets/google_sign_in_button/google_sign_in_button.dart
@@ -1,18 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
 import 'package:front/domain/services/auth/firebase_sign_in.dart';
-import 'package:front/domain/services/auth/google_sign_in.dart';
 import 'package:front/ini.dart';
 import 'package:front/util/logger.dart';
 import 'package:front/util/preview.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class GoogleSignInButton extends HookConsumerWidget {
-  const GoogleSignInButton({Key? key}) : super(key: key);
+  final double height;
+  const GoogleSignInButton({Key? key, this.height = 50}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return TextButton(
+    return SizedBox(
+      height: height,
+      child: SignInButton(
+        Buttons.Google,
         onPressed: () async {
           // googleを使ってFirebaseにサインイン
           await firebaseSignInWithGoogle();
@@ -22,11 +27,14 @@ class GoogleSignInButton extends HookConsumerWidget {
             logger.w("firebase sign in failed");
           }
         },
-        child: Text("Googleでサインイン"));
+      ),
+    );
   }
 }
 
 void main() {
   ini();
-  preview(const GoogleSignInButton());
+  preview(const Scaffold(
+    body: GoogleSignInButton(),
+  ));
 }
